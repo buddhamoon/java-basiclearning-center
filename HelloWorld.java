1e8ca74ad1da97d7b88174baba52da4da906d0ba
buddhaMoon <hy51hy@126.com>
2020-10-30 15:00:33 +0800

    auto

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println("Hello World");
+        int varone = 190;
+        for ( var i = 0; i < 10; i ++ ) {
+            System.out.println(varone + i);
+        }
+      
+        long pasWord = 20020L;
+        System.out.println(pasWord);
     }
 }
