f9151d4f1555f07119b7c783aeb92988e7e2f2c0
buddhaMoon <hy51hy@126.com>
2020-10-30 15:26:20 +0800

    auto

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main( String[] args ) {
         System.out.println("Hello World");
+
         int varone = 190;
         for ( var i = 0; i < 10; i ++ ) {
             System.out.println(varone + i);
@@ -8,5 +9,16 @@ public class HelloWorld {
       
         long pasWord = 20020L;
         System.out.println(pasWord);
+
+        String popSay = "你的名字";
+        int strLength = popSay.length();
+        long [] arrpopSay = { 1, 3, 0 }; 
+        if ( popSay == "名" ) {
+            System.out.println(popSay + " " + strLength);
+        } else {
+            System.out.println(strLength + " " + popSay);
+            System.out.println(popSay.charAt(3));
+            System.out.println(	arrpopSay + "数组某位：" +  arrpopSay[2]);
+        }
     }
 }
