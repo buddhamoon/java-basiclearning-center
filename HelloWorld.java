64e11480c2ee4949ddcaea8e1359385cf5e0f46a
buddhaMoon <hy51hy@126.com>
2020-11-03 18:45:16 +0800

    study

--- HelloWorld.java
+++ HelloWorld.java
@@ -28,5 +28,23 @@ public class HelloWorld {
             System.out.println(popSay.charAt(3));
             System.out.println(	arrpopSay + "数组某位：" +  arrpopSay[2]);
         }
+
+        // java 计算时只有浮点数能够触发浮点数（小数计算）
+        float a1num = 5F; float a2num = 3F;
+        System.out.println( a1num / a2num );
+
+        // 字符 + 操作会转换为数字类型，顺序为 int → long → float → double
+        char st01 = '就'; char st02 = 'p';
+        System.out.println( st01 + 8 );
+        System.out.println( st01 + st02 );
+
+        // 这里要注意，当要使用某个变量保存两个字符的 + 操作时，变量类型需要能够涵盖两个字符的叠加结果
+        int st03 = st01 + st02;
+        System.out.println( st03 );
+
+        // 字符串的拼接相对简单 ，字符串 + 操作单纯拼接（但要注意计算从左到右的先后顺序）。
+        System.out.println( "字符" + 99 + 1 );  // 结果 ：字符991
+        System.out.println( 99 + 1 + "字符" );  // 结果 ：100字符
+
     }
 }
